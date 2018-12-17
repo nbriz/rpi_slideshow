0a95e3ffb76aae633bb56c9c4d89f3f7c9e0274a
nbriz <nickbriz@gmail.com>
2018-12-17 16:13:28 -0600

    first draft of app

---
+++ server.js
@@ -0,0 +1,85 @@
+const { exec } = require('child_process')
+const fs = require('fs')
+const gdrive = require("./gdrive_frame/dist").default
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const port = process.argv[2] || 8000
+const chokidar = require('chokidar')
+const watcher = chokidar.watch(`${__dirname}/www/downloads`, {
+  ignored: /[\/\\]\./, persistent: true, ignoreInitial:true
+})
+
+// ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _
+// ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _
+
+function execLog(err,stdout,stderr){
+    if(err) return console.error(err)
+    console.log(`${stdout}`)
+    console.log(`${stderr}`)
+}
+
+function statusLog(str){
+    let d = '########'
+    for (let i = 0; i < str.length; i++) d+='#'
+    console.log(d)
+    console.log(`##  ${str}  ##`)
+    console.log(d)
+}
+
+function startChromium(log){
+    statusLog('launching chromium')
+    exec(`chromium-browser --kiosk http://localhost:${port}`, execLog)
+}
+
+function downloadImagesFromGdrive(socket){
+    let path = `${__dirname}/www/downloads`
+    gdrive(path)
+    // TODO run in callback or promise instead of timeout, ex:
+    // gdrive(path).then((files)=>socket.emit('images-downloaded',files))
+    setTimeout(()=>{
+        let filenames = []
+        fs.readdirSync(path).forEach(img=>filenames.push(img))
+        socket.emit('images-downloaded',filenames)
+    },3000)
+}
+
+function restartApp(){
+    statusLog('restarting app')
+    exec(`pm2 restart server`, execLog)
+}
+
+function restartComputer(){
+    statusLog('restarting computer')
+    exec(`reboot`, execLog)
+}
+
+function pullChanges(){
+    statusLog('pulling updates')
+    exec(`cd ${__dirname} ; git pull origin master`, execLog)
+}
+
+// ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _
+// ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _ . ~ * ~ . _
+
+app.use( express.static(__dirname+'/www') )
+
+startChromium()
+
+io.on('connection',function(socket){
+    socket.on('download-latest-images',()=>downloadImagesFromGdrive(socket))
+    socket.on('run-updates',pullChanges)
+    socket.on('restart-app',restartApp)
+    socket.on('restart-computer',restartComputer)
+    watcher.on('add',(path,stats)=>{
+        let pathArr = path.split('/')
+        let filename = pathArr[pathArr.length-1]
+        socket.emit('new-image',filename)
+    })
+})
+
+http.listen( port, function(err){
+    if(err) console.log(err)
+    else statusLog(`server is listening, visit http://localhost:${port}`)
+})
---
+++ www/main.js
@@ -0,0 +1,136 @@
+const socket = io()
+const gui = document.createElement('section')
+const canvas = document.createElement('canvas')
+const ctx = canvas.getContext('2d')
+const timer = { index:0, interval:0, speed:0.1 }
+let imgs = []
+
+setup()
+
+function setup(){
+    // setup canvas
+    applyCSS(canvas,{
+        position:"absolute", left:"0px", top:"0px", zIndex:"1"
+    })
+    canvas.addEventListener('click',()=>{
+        gui.style.display = "block"
+    })
+    document.body.appendChild( canvas )
+    window.addEventListener('resize',resize)
+
+    // setup GUI
+    applyCSS(gui,{
+        position:"absolute", left:"0px", top:"0px", zIndex:"2",
+        width:"100%", height:"100%", display:"none"
+    })
+    document.body.appendChild( gui )
+    createButton(gui,'run-updates')
+    createButton(gui,'restart-app')
+    createButton(gui,'restart-computer')
+
+    // setup socket listeners
+    socket.on('new-image',(filename)=>loadImage(filename))
+    socket.on('images-downloaded',(filenames)=>{
+        filenames.forEach(filename=>loadImage(filename))
+    })
+
+    // get things rolling
+    socket.emit('download-latest-images') // to downloads folder
+    downloadImagesAgainIn(5000)
+    resize()
+    draw()
+}
+
+function downloadImagesAgainIn(mili){
+    setTimeout(()=>{
+        socket.emit('download-latest-images') // to downloads folder
+        downloadImagesAgainIn(mili)
+    },mili)
+}
+
+/* -----------------------------------------------------------------------------
+---------------------------------------------------------------------- dom stuff
+----------------------------------------------------------------------------- */
+
+function applyCSS(element,cssObj){
+    for(prop in cssObj) element.style[prop] = cssObj[prop]
+}
+
+function createButton(gui,name){
+    let div = document.createElement('div')
+    div.addEventListener('click',()=>{
+        socket.emit(name)
+        gui.style.display = 'none'
+    })
+    div.textContent = name
+    applyCSS(div,{
+        color:'#fff', backgroundColor:'#c40477', textAlign:'center',
+        padding: '20px', fontSize: '28px', fontFamily: 'monospace',
+        cursor: 'pointer', borderBottom: '1px solid #fff'
+    })
+    gui.appendChild(div)
+}
+
+
+/* -----------------------------------------------------------------------------
+--------------------------------------------------------------- canvas animation
+----------------------------------------------------------------------------- */
+
+function resize(){
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+}
+
+function loadImage(filename){
+    let img = new Image()
+    img.onload = ()=>{
+        let names = imgs.map(img=>img.src)
+        if(!names.includes(img.src)) imgs.push(img)
+    }
+    img.src = `downloads/${filename}`
+}
+
+function matchFrameSize(pic){
+    return {
+        width:{
+            w:innerWidth,
+            h:pic.height * (innerWidth/pic.width)
+        },
+        height:{
+            w:pic.width * (innerHeight/pic.height),
+            h:innerHeight
+        }
+    }
+}
+
+function zoomIn(percent,pic,timer){
+    let match = matchFrameSize(pic).height
+    let maxWidth = match.w + (match.w*percent)
+    let rangeWidth = maxWidth - match.w
+    let maxHeight = match.h + (match.h*percent)
+    let rangeHeight = maxHeight - match.h
+    let w = match.w + (rangeWidth * timer.interval)
+    let h = match.h + (rangeHeight * timer.interval)
+    let x = (innerWidth-w) / 2
+    let y = -(h-match.h)/2
+    return { x, y, w, h }
+}
+
+function draw(){
+    requestAnimationFrame( draw )
+    if(imgs.length > 0){
+        let pic = imgs[timer.index]
+        let nxt = timer.index+1 >= imgs.length ?
+            imgs[0] : imgs[timer.index+1]
+        let t = zoomIn(0.25,pic,timer)
+        if( timer.interval < 1 ){
+            ctx.fillRect(0,0,innerWidth,innerHeight)
+            ctx.drawImage( pic, t.x, t.y, t.w, t.h )
+            timer.interval += timer.speed/60
+        } else {
+            timer.interval = 0
+            timer.index++
+            if(timer.index >= imgs.length) timer.index = 0
+        }
+    }
+}
